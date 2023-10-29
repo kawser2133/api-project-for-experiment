3d0021964f7d332fb6261e09058726524747f98e
Kawser Hamid <kawser2133@gmail.com>
2023-10-29 18:21:47 +0600

    Wrapper .net framework and .net core

---
+++ Project.API/Extensions/FrameworkLibraryWrapper.cs
@@ -0,0 +1,19 @@
+﻿using B_SDK;
+
+namespace Project.API.Extensions
+{
+    public class FrameworkLibraryWrapper
+    {
+        private BService frameworkLibrary;
+
+        public FrameworkLibraryWrapper()
+        {
+            frameworkLibrary = new BService();
+        }
+
+        public void CallFrameworkLibraryMethod()
+        {
+            frameworkLibrary.IsDeviceConnected();
+        }
+    }
+}
